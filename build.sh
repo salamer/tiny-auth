7156677f2e7ac5f0ba63f566040e13f7ca2c72c6
salamer <salamer_gaga@163.com>
2023-10-20 22:53:56 +0800

    update: update ui

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 pip install -r src/requirements.txt
 pip install gunicorn
+pip uninstall  Flask Jinja2
+pip install Flask Jinja2
+pip install flask-login --upgrade
+pip uninstall werkzeug
+pip install werkzeug==2.3.0
